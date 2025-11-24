738d823a1a8070061b33b1fe05c0e72ecca45078
KRST23 <stefanno.clash@gmail.com>
2025-11-24 15:31:41 -0300

    fortnite_players.py

---
+++ fortnite_players.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Fortnite_players_stats.csv")
+
+st.write("""
+# STATS FORTNITE PLAYERS.
+## Gráficos usando la base de datos estadísticab de Fortnite.
+""")
