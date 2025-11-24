91f9615be11e470196bf78718da296197dc96d7a
KRST23 <stefanno.clash@gmail.com>
2025-11-24 15:51:52 -0300

    Update fortnite_players.py

--- fortnite_players.py
+++ fortnite_players.py
@@ -6,7 +6,10 @@ df = pd.read_csv("Fortnite_players_stats.csv")
 
 st.write("""
 # STATS FORTNITE PLAYERS.
-## Gráficos usando la base de datos estadísticab de Fortnite.
+## Gráficos usando la base de datos estadística de Fortnite.
 """)
 
-df.head()
+df1 = df[['Solo score']]
+df1 = df1.sort_values(by='Solo score', ascending=False).head(50)
+fig1 = go.Figure(go.Bar(x=df1.index, y=df1['Solo score']))
+fig1.show()
