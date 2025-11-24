01a72245cd5352b552bbb249cd8b35d192e4a573
KRST23 <stefanno.clash@gmail.com>
2025-11-24 17:11:48 -0300

    Update fortnite_players.py

--- fortnite_players.py
+++ fortnite_players.py
@@ -9,7 +9,7 @@ st.write("""
 ## Gráficos usando la base de datos estadística de Fortnite.
 """)
 
-solos_top20 = df[['Player','Solo score']].sort_values('Solo score',ascending=False).head(20)
+solos_top10 = df[['Player','Solo score']].sort_values('Solo score',ascending=False).head(20)
 
 #df1 = df[['Solo score']]
 #df1 = df1.sort_values(by='Solo score', ascending=False).head(10)
@@ -18,10 +18,10 @@ solos_top20 = df[['Player','Solo score']].sort_values('Solo score',ascending=Fal
 fig, ax = plt.subplots(figsize=(12, 6)) 
 
 
-ax.bar(solos_top20.index.astype(str), solos_top20['Solo score'], color='#3498db') 
+ax.bar(solos_top10.index.astype(str), solos_top10['Solo score'], color='#3498db') 
 
 
-ax.set_title('Top 50 Solo Scores')
+ax.set_title('Top 10 Solo Scores')
 ax.set_xlabel('Índice / Jugador')
 ax.set_ylabel('Score')
 plt.xticks(rotation=90, fontsize=8) # Rotamos etiquetas del eje X si son nombres largos
@@ -41,7 +41,7 @@ for spine in ax.spines.values():
     spine.set_visible(False)
 
 
-max_val = solos_top20['Solo score'].max()
+max_val = solos_top10['Solo score'].max()
 # Definimos el paso: cada 1 millón (1,000,000)
 step = 1_000_000
 
