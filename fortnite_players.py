297579d66677a18f006e053f860251c26977ad01
KRST23 <stefanno.clash@gmail.com>
2025-11-24 16:51:55 -0300

    Update fortnite_players.py

--- fortnite_players.py
+++ fortnite_players.py
@@ -9,24 +9,6 @@ st.write("""
 ## Gráficos usando la base de datos estadística de Fortnite.
 """)
 
-df1 = df[['Solo score']]
-df1 = df1.sort_values(by='Solo score', ascending=False).head(10)
-
-
-fig, ax = plt.subplots(figsize=(12, 6)) 
-
-ax.bar(df1.index.astype(str), df1['Solo score'], color='#06EAB9') 
-
-
-ax.set_title('Top 10 Solo Scores')
-ax.set_xlabel('Índice / Jugador')
-ax.set_ylabel('Score')
-plt.xticks(rotation=90, fontsize=8) 
-
-
-st.pyplot(fig)
-
-
 
 --------------------- nuevo codigo ------------
 
